2f980931ba99e631c7d1d423b4d115fed68f4fd9
Renggeng Zheng <rzhengcontacts@gmail.com>
2026-04-01 18:55:44 -0400

    checking context length boundaries

--- smolbench/evals/openrouter.py
+++ smolbench/evals/openrouter.py
@@ -14,8 +14,8 @@ from smolbench.evals import Answer, QnA, Quiz, Marks
 
 OPENROUTER_API_KEY: str = os.getenv("OPENROUTER_API_KEY", None)
 URL: str = "https://openrouter.ai/api/v1/chat/completions"
-OPENROUTER_DEBUG: bool = bool(int(os.getenv("OPENROUTER_DEBUG", "0")))
-OPENROUTER_DEBUG_RESPONSE: bool = bool(int(os.getenv("OPENROUTER_DEBUG_RESPONSE", "0")))
+OPENROUTER_INFO: bool = bool(int(os.getenv("OPENROUTER_INFO", "0")))
+OPENROUTER_INFO_RESPONSE: bool = bool(int(os.getenv("OPENROUTER_INFO_RESPONSE", "0")))
 OPENROUTER_MAX_PARALLEL_REQUESTS: int = int(
     os.getenv("OPENROUTER_MAX_PARALLEL_REQUESTS", "8")
 )
@@ -36,8 +36,24 @@ def _is_retryable_request_error(err: requests.exceptions.RequestException) -> bo
     return True
 
 
+def get_model_context_length(model: str) -> int:
+    """Fetches the model context window from segments."""
+    #TODO: implement this fn
+    response: Dict[str, Any] = requests.get(
+        url=f"https://openrouter.ai/api/v1/models/{model}/endpoints",
+        headers={
+            "Authorization": f"Bearer {OPENROUTER_API_KEY}",
+        },
+        timeout=120,
+    ).json()
+
+    # pick the first available endpoint
+    ctx: int = response["data"]["endpoints"][0]["context_length"]
+    return ctx
+
 def query(
-    prompt: str, model: str, seed: int, extra_args: Optional[Dict[str, Any]] = None
+    prompt: str, model: str, seed: int, context_length: int = 0,
+    extra_args: Optional[Dict[str, Any]] = None
 ) -> str:
     """
     Queries a model using openrouter.
@@ -48,6 +64,12 @@ def query(
         The content posed to the LLM we expect an answer from.
     model:
         The model to evaluate on OpenRouter.
+    seed:
+        Seed for LLM output.
+    context_length:
+        Context length of LLM model.
+    extra_args:
+        Extra args for `json=<slug>` of requests to get certain LLM behavior.
 
     Returns
     -------
@@ -81,12 +103,18 @@ def query(
 
             response.raise_for_status()
             body = response.json()
-            if OPENROUTER_DEBUG and OPENROUTER_DEBUG_RESPONSE:
-                logging.debug(body)
+            if OPENROUTER_INFO and OPENROUTER_INFO_RESPONSE:
+                logging.info(body)
 
             if body["choices"][0]["message"]["content"] is None:
                 logging.warning("Body returned none value: \n" f"{body}")
                 return ""
+            if (tokens := body["usage"]["total_tokens"]) > context_length:
+                raise ValueError(
+                    f"Response:\n{body}\n was {tokens} > {context_length}"
+                )
+            if OPENROUTER_INFO:
+                logging.info(f"Response:\n{body}\n was {tokens} <= {context_length}")
             return body["choices"][0]["message"]["content"]
 
         # Attempts to retry exceptions if possible.
@@ -115,25 +143,29 @@ def evaluate(
     incorrect: int = 0
     invalid: int = 0
 
+    # Gets the model context length.
+    ctx_len: int = get_model_context_length(model)
     # Batches the requests across workers.
     max_workers: int = max(1, min(len(quiz), OPENROUTER_MAX_PARALLEL_REQUESTS))
     responses: list[str] = Parallel(n_jobs=max_workers, prefer="threads")(
-        delayed(query)(q.prompt, model, seed, extra_args) for q in quiz
+        delayed(query)(
+            q.prompt, model, seed, ctx_len, extra_args=extra_args
+        ) for q in quiz
     )
 
     # Marks the responses after all requests complete.
     q: QnA
     response: str
     for q, response in zip(quiz, responses):
-        if OPENROUTER_DEBUG:
-            logging.debug(correct, incorrect, invalid)
+        if OPENROUTER_INFO:
+            logging.info(correct, incorrect, invalid)
         # Tracks if the response given is "nonsensical."
         try:
             response: Answer = q.condition(response)
         except ValueError as e:
             invalid += 1
-            if OPENROUTER_DEBUG:
-                logging.debug(e)
+            if OPENROUTER_INFO:
+                logging.info(e)
             continue
 
         # Marks any sensical answers and updates scoring.
